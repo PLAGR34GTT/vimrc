04aead5f403c77e965df802f5ec575b2fbc404ba
PLAGR34GTT <schooly8@hotmail.com>
2017-02-11 21:55:33 +0000

    oh my gosh

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,3 +1,29 @@
+cd ~/.vim_runtime/lib
+
+git clone git@github.com:tpope/vim-pathogen.git
+git clone git@github.com:scrooloose/nerdtree.git
+git clone git@github.com:kien/ctrlp.vim.git
+git clone git@github.com:mileszs/ack.vim.git
+git clone git@github.com:vim-airline/vim-airline.git
+git clone git@github.com:Valloric/YouCompleteMe.git
+git clone git@github.com:vim-syntastic/syntastic.git
+git clone git@github.com:tiagofumo/vim-nerdtree-syntax-highlight.git
+git clone git@github.com:Lokaltog/vim-distinguished.git
+
+git clone git@github.com:mattn/emmet-vim.git
+git clone git@github.com:pangloss/vim-javascript.git
+git clone git@github.com:mxw/vim-jsx.git
+
+git clone git@github.com:jiangmiao/auto-pairs.git
+git clone git@github.com:tpope/vim-surround.git
+git clone git@github.com:ntpeters/vim-better-whitespace.git
+git clone git@github.com:plasticboy/vim-markdown.git
+git clone git@github.com:airblade/vim-gitgutter.git
+git clone git@github.com:tpope/vim-fugitive.git
+git clone git@github.com:terryma/vim-multiple-cursors.git
+git clone git@github.com:tpope/vim-commentary.git
+git clone git@github.com:tpope/vim-cucumber.git
+
 cd ~/.vim_runtime
 
 echo 'set runtimepath+=~/.vim_runtime
