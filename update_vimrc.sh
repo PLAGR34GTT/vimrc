9f9c71e328acbdaab2cab8429bc7e44ced2ebe8f
PLAGR34 <schooly8@hotmail.com>
2017-02-18 12:58:41 +0000

    Add update vimrc script;

---
+++ update_vimrc.sh
@@ -0,0 +1,17 @@
+cd ~/.vim_runtime
+
+echo 'set runtimepath+=~/.vim_runtime
+
+source ~/.vim_runtime/lib/vim-pathogen/autoload/pathogen.vim
+source ~/.vim_runtime/vimrcs/pathogen.vim
+source ~/.vim_runtime/vimrcs/basic.vim
+source ~/.vim_runtime/vimrcs/youcompleteme.vim
+source ~/.vim_runtime/vimrcs/nerdtree.vim
+source ~/.vim_runtime/vimrcs/syntastic.vim
+source ~/.vim_runtime/vimrcs/vim-javascript.vim
+source ~/.vim_runtime/vimrcs/editorconfig.vim
+source ~/.vim_runtime/vimrcs/airline.vim
+source ~/.vim_runtime/vimrcs/ctrlp.vim
+source ~/.vim_runtime/vimrcs/devicons.vim' > ~/.vimrc
+
+echo ".vimrc updated."
